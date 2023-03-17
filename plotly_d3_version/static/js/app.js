9e19cc52b51d93cd34371d01cbb5c18027f17a5d
KellyPared <kellschu333@gmail.com>
2023-03-17 14:24:31 -0400

    update

--- plotly_d3_version/static/js/app.js
+++ plotly_d3_version/static/js/app.js
@@ -1,101 +1,16 @@
-var margin  = {top: 20, right: 20, bottom: 100, left: 60},
-        width   = 600 - margin.left - margin.right,
-        height  = 400 - margin.top - margin.bottom,
-        x       = d3.scale.ordinal().rangeRoundBands([0,width], 0.5),
-        y       = d3.scale.linear().range([height,0]);
+samples_url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-        var xAxis   = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
+//promise
+const dataPromise = d3.json(samples_url)
+
+//console
+console.log(dataPromise)
+d3.json(samples_url).then(function (samples_data){
+    console.log(samples_data);
 
 
-var yAxis   = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .ticks(5)
-      .innerTickSize(-width)
-      .outerTickSize(0)
-      .tickPadding(10);
 
-d3.json("data/samples.json", function (data)
-{
-  x.domain(data.map(function (d)
-  {
-      return d.name;
-  }));
-
-  y.domain([0, d3.max(data, function (d)
-  {
-      return d.wc;
-  })]);
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0, " + height + ")")
-      .call(xAxis)
-      .selectAll("text")
-      .style("text-anchor", "middle")
-      .attr("dx", "-0.5em")
-      .attr("dy", "-.55em")
-      .attr("y", 30)
-      .attr("transform", "rotate(0)" );
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 5)
-      .attr("dy", "0.8em")
-      .attr("text-anchor", "end")
-      .text("Word Count");
-
-  svg.selectAll("bar")
-      .data(data)
-      .enter()
-      .append("rect")
-      .style("fill", "orange")
-      .attr("x", function(d)
-      {
-          return x(d.name);
-      })
-      .attr("width", x.rangeBand())
-      .attr("y", function (d)
-      {
-          return y(d.wc);
-      })
-      .attr("height", function (d)
-      {
-          return height - y(d.wc);
-      })
-      {
-  const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", y.range()[1])
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "10")
-      .attr("text-anchor", "end");
-
-  const bar = svg.selectAll("g")
-    .data(data)
-    .join("g")
-      .attr("transform", (d, i) => `translate(0,${y(i)})`);
-
-  bar.append("rect")
-      .attr("fill", "steelblue")
-      .attr("width", x)
-      .attr("height", y.bandwidth() - 1);
-
-  bar.append("text")
-      .attr("fill", "white")
-      .attr("x", d => x(d) - 3)
-      .attr("y", (y.bandwidth() - 1) / 2)
-      .attr("dy", "0.35em")
-      .text(d => d);
-
-  return svg.node();
-}
-
+// horizontal bar chart w/ a dropdown menu to display the top 10 OTUs found in that individual.
 var bar_data = [{
     type: 'bar',
     x: otu_ids,
@@ -105,7 +20,7 @@ var bar_data = [{
   
   Plotly.newPlot('bar', bar_data);
 
-
+//bubble chart that displays each sample.
   var trace1 = {
     x: otu_ids,
     y: sample_values,
@@ -125,3 +40,6 @@ var bar_data = [{
   };
   
   Plotly.newPlot('bubble', bubble_data, layout);
+
+
+}).catch(console.log('try again'))
